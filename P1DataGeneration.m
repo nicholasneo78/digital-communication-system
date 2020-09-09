d74d01fb2328682b19f2eb4acc1a87263d65b6cd
ychoux <ychouxe@gmail.com>
2020-09-09 20:25:02 +0800

    Added relevant comments

--- P1DataGeneration.m
+++ P1DataGeneration.m
@@ -1,88 +1,64 @@
 %[1-1]
-% generate random binary digits (0 or 1) 
+%Generate random binary digits (0 or 1) 
 noOfBits = 1024;
 inputData = randi([0, 1], [1, noOfBits]);
-%inputData % test data generated
 
 %[1-2]
-% convert binary digits to +/- 1 (1->+1, 0->-1) => Data for transmission
-% replace 0 to -1
+%Convert binary digits to +/- 1 (1->+1, 0->-1) => Data for transmission
+%Replace 0 to -1
 inputData(inputData==0) = -1;
-%inputData % test data generated
 
-zeroMean = 0;
+%[1-14]
+%Simulation for different SNR values
 SNR = 0:5:50; %Loop from 0 to 50 (in multiples of 5)
-S=1;
-bitErrorRate = zeros(1,11); %Initialise an array of 1-by-11 zeros
-SNRvalues = zeros(1,11);
-arrayIndex = 1;
 
-for i = SNR
-    noOfErrors = 0;
-    N=S./(10.^(i./10));
-    outputSignal = awgn(inputData,i,N);
-    threshold = 0;
-    outputSignal(outputSignal>=0) = 1;
-    outputSignal(outputSignal<0) = 0;
-    plot(outputSignal);
-    for count=1:noOfBits
+%[1-6]
+%Assume input data has unit power
+S=1; 
+
+%[1-16]
+bitErrorRate = zeros(1,11); %Initialise an array of 1-by-11 zeros to store error rate
+SNRvalues = zeros(1,11); %Initialise an array of 1-by-11 zeros to store SNR values
+arrayIndex = 1; %To count the array index for storage
+
+for i = SNR %Loop from 0 to 50 (in multiples of 5)
+    %[1-11]
+    noOfErrors = 0; %Count number of errors for each SNR
+    
+    %[1-6]
+    N=S./(10.^(i./10)); %Obtain noise variance (10log10 = S/N)
+    
+    %[1-3 && 1-7]
+    %AWGN function applys White Gaussian Noise to inputData
+    outputSignal = awgn(inputData,i,N); %i=SNR, N=noise variance
+    
+    %[1-8]
+    %Convert outputSignal to 0/1
+    %(outputSignal >= 0) -> 1
+    %(outputSignal < 0) -> 0
+    threshold = 0; 
+    outputSignal(outputSignal>=threshold) = 1;
+    outputSignal(outputSignal<threshold) = 0;
+    
+    %[1-9]
+    %Conpute bit error rate
+    for count=1:noOfBits %Loop from 1 to 1024
+        %If inputData > 0 but outputSignal < 0, error
+        %If inputData <= 0 but outputSignal >= 0, error
         if (inputData(count) > threshold && outputSignal(count) == 0) || (inputData(count) <= threshold && outputSignal(count) == 1)
             noOfErrors = noOfErrors + 1;
         end
     end
-    SNRvalues(arrayIndex)=i;
-    bitErrorRate(arrayIndex)= noOfErrors/noOfBits; %Store bitErrorRate into array
+    %[1-16]
+    SNRvalues(arrayIndex)=i; %Store current SNR value into array
+    bitErrorRate(arrayIndex)= noOfErrors/noOfBits; %Store the bitErrorRate into array
     arrayIndex=arrayIndex+1;
 end
 
-%Plot
+%[1-15]
+%Plot the results
 semilogy(SNRvalues, bitErrorRate);
 axis([0 50 -1 1])
 title("Plot of Bit Error Rate vs Signal to Noise Ratio");
 xlabel('E_{b}/N_{0}') ;
 ylabel('P_{e}') ;
-
-%{
-
-%[1-3, 1-4]
-% generate equal number of noise samples
-S = 1; %Assume signal (input data) has unit power
-meanNoise = 0; % zero mean
-SNR = 0:5:50; %Loop from 0 to 50 (in multiples of 5)
-bitErrorRateOutput = zeros(1,11); %Initialise an array of 1-by-11 zeros
-SNRAxis = zeros(1,11); %Initialise an array of 1-by-11 zeros
-counter = 1; %Counter for array index
-
-for i=SNR 
-    varianceNoise = S./(10.^(i./10)); % unit variance
-    % Follow notes
-    receivedSignal = calculateNoise(inputData, varianceNoise);
-    threshold = 0;
-    receivedSignal(receivedSignal>=0) = 1;
-    receivedSignal(receivedSignal<0) = 0;
-    bitError = 0;
-    for count=1 : noOfBits
-        if (inputData(count) > threshold && receivedSignal(count) == 0) || (inputData(count) <= threshold && receivedSignal(count) == 1)
-            bitError = bitError + 1;
-        end
-    end    
-    bitErrorRateOutput (counter)= bitError/noOfBits; %Store bitErrorRate into array
-    SNRAxis(counter) = i; %Store SNR value
-    counter=counter+1;
-end
-
-%Plot
-semilogy(SNRAxis, bitErrorRateOutput);
-axis([0 50 -1 1])
-title("Plot of Bit Error Rate vs Signal to Noise Ratio");
-xlabel('E_{b}/N_{0}') ;
-ylabel('P_{e}') ;
-
-function receivedSignal = calculateNoise(inputData, varianceNoise)
-    % get noise (according to normal distribution)
-    noise = (sqrt(varianceNoise).*randn(1,1024)); %Generate white gaussian noise
-    %noise % look at the noise values generated with mean=0 and variance=1
-    receivedSignal = noise+inputData; %Adding generated noise with generated random data
-end
-
-%}
